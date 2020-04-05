1fbf5ebdeb563b617b7da39e0d92807deb89aaef
MaevaPasteur <maeva.pasteur.pro@gmail.com>
2020-04-05 13:35:52 +0200

    Feat pwa

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,10 @@
+if('serviceWorker' in navigator){
+    navigator.serviceWorker.register('sw.js')
+        .then(reg => console.log('service worker registered'))
+        .catch(err => console.log('service worker not registered', err));
+}
+
 document.addEventListener('DOMContentLoaded', () => {
-
-
 
     /*******************************************************************************************************************
      * Déclaration des variables
